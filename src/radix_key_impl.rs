966e80e5d2a98a53404922a08f6213724657fd8e
Nathan Essex <nathan@essex.id.au>
2024-02-04 14:42:51 +0900

    Faster default implementations of RadixKey

--- src/radix_key_impl.rs
+++ src/radix_key_impl.rs
@@ -3,7 +3,7 @@ use crate::RadixKey;
 impl RadixKey for u8 {
     const LEVELS: usize = 1;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, _: usize) -> u8 {
         *self
     }
@@ -12,36 +12,60 @@ impl RadixKey for u8 {
 impl RadixKey for u16 {
     const LEVELS: usize = 2;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
-        (self >> (level * 8)) as u8
+        debug_assert!(level < Self::LEVELS);
+
+        if cfg!(target_endian="little") {
+            unsafe { (self as *const Self as *const u8).wrapping_offset(level as isize).read() }
+        } else {
+            (self >> (level * 8)) as u8
+        }
     }
 }
 
 impl RadixKey for u32 {
     const LEVELS: usize = 4;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
-        (self >> (level * 8)) as u8
+        debug_assert!(level < Self::LEVELS);
+
+        if cfg!(target_endian="little") {
+            unsafe { (self as *const Self as *const u8).wrapping_offset(level as isize).read() }
+        } else {
+            (self >> (level * 8)) as u8
+        }
     }
 }
 
 impl RadixKey for u64 {
     const LEVELS: usize = 8;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
-        (self >> (level * 8)) as u8
+        debug_assert!(level < Self::LEVELS);
+
+        if cfg!(target_endian="little") {
+            unsafe { (self as *const Self as *const u8).wrapping_offset(level as isize).read() }
+        } else {
+            (self >> (level * 8)) as u8
+        }
     }
 }
 
 impl RadixKey for u128 {
     const LEVELS: usize = 16;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
-        (self >> (level * 8)) as u8
+        debug_assert!(level < Self::LEVELS);
+
+        if cfg!(target_endian="little") {
+            unsafe { (self as *const Self as *const u8).wrapping_offset(level as isize).read() }
+        } else {
+            (self >> (level * 8)) as u8
+        }
     }
 }
 
@@ -49,9 +73,15 @@ impl RadixKey for u128 {
 impl RadixKey for usize {
     const LEVELS: usize = 2;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
-        (self >> (level * 8)) as u8
+        debug_assert!(level < Self::LEVELS);
+
+        if cfg!(target_endian="little") {
+            unsafe { (self as *const Self as *const u8).wrapping_offset(level as isize).read() }
+        } else {
+            (self >> (level * 8)) as u8
+        }
     }
 }
 
@@ -59,9 +89,15 @@ impl RadixKey for usize {
 impl RadixKey for usize {
     const LEVELS: usize = 4;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
-        (self >> (level * 8)) as u8
+        debug_assert!(level < Self::LEVELS);
+
+        if cfg!(target_endian="little") {
+            unsafe { (self as *const Self as *const u8).wrapping_offset(level as isize).read() }
+        } else {
+            (self >> (level * 8)) as u8
+        }
     }
 }
 
@@ -69,16 +105,22 @@ impl RadixKey for usize {
 impl RadixKey for usize {
     const LEVELS: usize = 8;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
-        (self >> (level * 8)) as u8
+        debug_assert!(level < Self::LEVELS);
+
+        if cfg!(target_endian="little") {
+            unsafe { (self as *const Self as *const u8).wrapping_offset(level as isize).read() }
+        } else {
+            (self >> (level * 8)) as u8
+        }
     }
 }
 
 impl<const N: usize> RadixKey for [u8; N] {
     const LEVELS: usize = N;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
         self[level]
     }
@@ -87,7 +129,7 @@ impl<const N: usize> RadixKey for [u8; N] {
 impl RadixKey for i8 {
     const LEVELS: usize = 1;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, _: usize) -> u8 {
         (*self ^ i8::MIN) as u8
     }
@@ -96,7 +138,7 @@ impl RadixKey for i8 {
 impl RadixKey for i16 {
     const LEVELS: usize = 2;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
         ((self ^ i16::MIN) >> (level * 8)) as u8
     }
@@ -105,7 +147,7 @@ impl RadixKey for i16 {
 impl RadixKey for i32 {
     const LEVELS: usize = 4;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
         ((self ^ i32::MIN) >> (level * 8)) as u8
     }
@@ -114,7 +156,7 @@ impl RadixKey for i32 {
 impl RadixKey for i64 {
     const LEVELS: usize = 8;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
         ((self ^ i64::MIN) >> (level * 8)) as u8
     }
@@ -123,7 +165,7 @@ impl RadixKey for i64 {
 impl RadixKey for i128 {
     const LEVELS: usize = 16;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
         ((self ^ i128::MIN) >> (level * 8)) as u8
     }
@@ -133,7 +175,7 @@ impl RadixKey for i128 {
 impl RadixKey for isize {
     const LEVELS: usize = 2;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
         ((self ^ isize::MIN) >> (level * 8)) as u8
     }
@@ -143,7 +185,7 @@ impl RadixKey for isize {
 impl RadixKey for isize {
     const LEVELS: usize = 4;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
         ((self ^ isize::MIN) >> (level * 8)) as u8
     }
@@ -153,7 +195,7 @@ impl RadixKey for isize {
 impl RadixKey for isize {
     const LEVELS: usize = 8;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
         ((self ^ isize::MIN) >> (level * 8)) as u8
     }
@@ -162,7 +204,7 @@ impl RadixKey for isize {
 impl RadixKey for f32 {
     const LEVELS: usize = 4;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
         let mut s = self.to_bits() as i32;
 
@@ -175,7 +217,7 @@ impl RadixKey for f32 {
 impl RadixKey for f64 {
     const LEVELS: usize = 8;
 
-    #[inline]
+    #[inline(always)]
     fn get_level(&self, level: usize) -> u8 {
         let mut s = self.to_bits() as i64;
         s ^= (((s >> 63) as u64) >> 1) as i64;
